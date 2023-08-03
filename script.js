b8664dea755b5265030598e46649c133fb61ccc4
JaiminPansuriya <138955259+JaiminPansuriya@users.noreply.github.com>
2023-08-03 16:27:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,198 @@
+// Game  Constants & Varible
+
+let inputDir = {
+  x: 0,
+  y: 0,
+};
+const foodSound = new Audio("food.mp3");
+const gameOverSound = new Audio("gameover.mp3");
+const moveSound = new Audio("move.mp3");
+const musicSound = new Audio("music.mp3");
+let speed = 5;
+let score = 0;
+let lastPaintTime = 0;
+let snakeArr = [
+  {
+    x: 13,
+    y: 15,
+  },
+];
+food = {
+  x: 6,
+  y: 7,
+};
+
+// Function to start the game and music
+function startGame() {
+  musicSound.play(); // Start playing the music
+  main(0); // Start the game loop
+}
+
+// Game Function
+function main(ctime) {
+  window.requestAnimationFrame(main);
+  // console.log(ctime);
+  if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
+    return;
+  }
+  lastPaintTime = ctime;
+  gameEngine();
+}
+
+function isCollide(snake) {
+  //If you bump into yourself
+  for (let i = 1; i < snakeArr.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+      return true;
+    }
+  }
+
+  //If you bump into the fall
+  if (
+    snake[0].x >= 18 ||
+    snake[0].x <= 0 ||
+    snake[0].y >= 18 ||
+    snake[0].y <= 0
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function gameEngine() {
+  //Paart 1: Updating the snake array & Food
+
+  if (isCollide(snakeArr)) {
+    gameOverSound.play();
+    musicSound.pause();
+    inputDir = {
+      x: 0,
+      y: 0,
+    };
+    alert("Game Over. Press any key to play again!");
+
+    snakeArr = [
+      {
+        x: 13,
+        y: 15,
+      },
+    ];
+    musicSound.play();
+    score = 0;
+  }
+
+  //If you have eaten the food,increment the scoRE and regenerate the food
+
+  if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
+    foodSound.play();
+    score += 1;
+    if (score > hiscoreval) {
+      hiscoreval = score;
+      localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+      hiscoreBox.innerHTML = "High Score: " + hiscoreval;
+    }
+    scoreBox.innerHTML = "Score: " + score;
+    snakeArr.unshift({
+      x: snakeArr[0].x + inputDir.x,
+      y: snakeArr[0].y + inputDir.y,
+    });
+    let a = 2;
+    let b = 14;
+    food = {
+      x: 2 + Math.round(a + (b - a) * Math.random()),
+      y: 2 + Math.round(a + (b - a) * Math.random()),
+    };
+  }
+
+  //Moving the Snake
+
+  for (let i = snakeArr.length - 2; i >= 0; i--) {
+    snakeArr[i + 1] = { ...snakeArr[i] };
+  }
+
+  snakeArr[0].x += inputDir.x;
+  snakeArr[0].y += inputDir.y;
+
+  //Paart 2: Display the snake and food
+
+  // Display the Snake
+
+  board.innerHTML = "";
+  snakeArr.forEach((e, index) => {
+    snakeElement = document.createElement("div");
+    snakeElement.style.gridRowStart = e.y;
+    snakeElement.style.gridColumnStart = e.x;
+    if (index === 0) {
+      snakeElement.classList.add("head");
+    } else {
+      snakeElement.classList.add("snake");
+    }
+    board.appendChild(snakeElement);
+  });
+
+  // Display the Food
+
+  foodElement = document.createElement("div");
+  foodElement.style.gridRowStart = food.y;
+  foodElement.style.gridColumnStart = food.x;
+  foodElement.classList.add("food");
+  board.appendChild(foodElement);
+}
+
+// Main Logic
+
+let hiscore = localStorage.getItem("hiscore");
+if (hiscore === null) {
+  hiscoreval = 0;
+  localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+} else {
+  hiscoreval = JSON.parse(hiscore);
+  hiscoreBox.innerHTML = "High Score: " + hiscore;
+}
+
+window.requestAnimationFrame(main);
+window.addEventListener("keydown", (e) => {
+  e.preventDefault();
+  inputDir = {
+    x: 0,
+    y: 1,
+  }; // Start the Game
+  moveSound.play();
+  switch (e.key) {
+    case "ArrowUp":
+      console.log("ArrowUp");
+      inputDir.x = 0;
+      inputDir.y = -1;
+      break;
+
+    case "ArrowDown":
+      console.log("ArrowDown");
+      inputDir.x = 0;
+      inputDir.y = 1;
+      break;
+
+    case "ArrowLeft":
+      console.log("ArrowLeft");
+      inputDir.x = -1;
+      inputDir.y = 0;
+      break;
+
+    case "ArrowRight":
+      console.log("ArrowRight");
+      inputDir.x = 1;
+      inputDir.y = 0;
+      break;
+
+    default:
+      break;
+  }
+  if (
+    e.key === "ArrowUp" ||
+    e.key === "ArrowDown" ||
+    e.key === "ArrowLeft" ||
+    e.key === "ArrowRight"
+  ) {
+    // Start the game only if an arrow key is pressed
+    startGame();
+  }
+});
